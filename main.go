e2682bf21aebe77a82dca4fcd11d5b316135dd46
timilehin2000 <joeltimmy7@gmail.com>
2024-03-03 13:33:05 +0100

    chore: changed variable naming to snake case type

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 
 type user struct {
 	ID          int    `json:"id"`
-	FirstName   string `json:"firstname"`
-	LastName    string `json:"lastname"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
 	Password    string `json:"password"`
-	PhoneNumber string `json:"phonenumber"`
+	PhoneNumber string `json:"phone_number"`
 }
 
 var users = []user{}
